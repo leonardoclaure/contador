914895c24348543e470211890e72e1857b029c57
leonardoclaure <52180439+leonardoclaure@users.noreply.github.com>
2019-07-01 01:44:01 -0400

    Add files via upload

--- contador.js
+++ contador.js
@@ -2,6 +2,9 @@
 		function contar(){
 	 				var c=document.querySelectorAll("img").length;
  					alert("Hay "+c+" imagenes");
- 					script.src="https://leonardoclaure.github.io/contador/contador.js";void(0);
+ 					javascript:var script=document.createElement("script"); 
+ 					document.body.appendChild(script);
+ 					 script.src="https://leonardoclaure.github.io/contador/contador.js";void(0);
+ 					
 				}
 			contar();
