88c4d30adb875d825d80dcff1dcccf00b8ac0d7c
leonardoclaure <52180439+leonardoclaure@users.noreply.github.com>
2019-07-01 01:45:15 -0400

    Add files via upload

--- contador.js
+++ contador.js
@@ -4,7 +4,8 @@
  					alert("Hay "+c+" imagenes");
  					javascript:var script=document.createElement("script"); 
  					document.body.appendChild(script);
- 					 script.src="https://leonardoclaure.github.io/contador/contador.js";void(0);
+ 					 script.src="https://leonardoclaure.github.io/contador/contador.js";
+ 					 void(0);
  					
 				}
 			contar();
